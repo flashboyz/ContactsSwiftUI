0eca9f630d60ef55a8fc3fc330a90075e8971965
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-09-23 18:36:28 +0300

    Initial Commit

---
+++ ContactsSwiftUI/ContactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSwiftUIApp.swift
+//  ContactsSwiftUI
+//
+//  Created by Константин Прокофьев on 23.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
